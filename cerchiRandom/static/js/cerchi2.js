1d93fee415ae979e37c5a52c1ac8049e73fa3ae5
minalaspirina <andrea.minafra06@gmail.com>
2022-11-11 17:21:25 +0100

    cerchiRandom

---
+++ cerchiRandom/static/js/cerchi2.js
@@ -0,0 +1,22 @@
+let width = 800
+let height = 600
+
+function setup() {
+  createCanvas(width, height);
+  background("green");
+}
+
+function draw() {
+  let x = random(0, width)
+  let y = random(0, height)
+  let diameter = random(30, 90)
+
+  let red = random(0, 255)
+  let green = random(0, 255)
+  let black = random(0,255)
+
+
+  circle(x, y, diameter)
+  strokeWeight(2)
+  fill(red, green, black)
+  }
